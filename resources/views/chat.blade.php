e53eaabe93a4c27e7b3c868ee866b7eba43504c4
hussein <eng.husseinad@gmail.com>
2018-04-08 18:17:16 +0200

    orginal pusher chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hussein
+ * Date: 4/8/18
+ * Time: 12:38 PM
+ */?>
+
+
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container" >
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Chats</div>
+
+                    <div class="panel-body">
+                        <chat-messages :messages="messages"></chat-messages>
+                    </div>
+                    <div class="panel-footer">
+                        <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
